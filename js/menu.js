e241fe153db880881db911a7d04c4c38fc7674e4
Anton Semenikhin <protest_the_hero@mail.ru>
2022-03-30 13:00:02 +0300

    add footer and media

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,16 @@
-'use strict'
-
-const menu = document.querySelector('.header-burger__menu')
-
-document.addEventListener('click', (e) => {
+const menuBurger = document.querySelector('.header-burger__menu')
+const btnBurger = document.querySelector('.burger-button')
+btnBurger.addEventListener('click', (e) => {
 	e.preventDefault()
 	if (e.target.closest('.burger-button') || e.target.matches('.header__nav--hidden a')) {
-		menu.classList.toggle('visible')
+		menuBurger.classList.toggle('visible')
 		document.body.classList.toggle('disabled')
 	}
 })
 
 window.addEventListener('resize', () => {
 	if (innerWidth > 991) {
-		menu.classList.remove('visible')
+		menuBurger.classList.remove('visible')
 		document.body.classList.remove('disabled')
 	}
 })
